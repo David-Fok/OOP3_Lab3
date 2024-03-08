307ed42419bf13055b08c90b4b4e1131426d28dc
David-Fok <foktatchun@gmail.com>
2024-03-08 16:17:55 -0700

    implementation

--- src/implementations/Dictionary.java
+++ src/implementations/Dictionary.java
@@ -1,7 +1,6 @@
 package implementations;
 
 import java.util.ArrayList;
-
 import exceptions.DuplicateKeyException;
 import utilities.DictionaryADT;
 
@@ -14,4 +13,37 @@ public class Dictionary<K,V> implements DictionaryADT<K,V>
 	private ArrayList<K> keys;
 	private ArrayList<V> values;
 
+	public Dictionary() {
+		keys = new ArrayList<>(DEFAULT_SIZE);
+		values = new ArrayList<>(DEFAULT_SIZE);
+	}
+	
+	public ArrayList<K> getKeys() {
+		return keys;
+	}
+	public ArrayList<V> getValues() {
+		return values;
+	}
+	
+	public void insert(K key, V value)throws DuplicateKeyException{
+		if (keys.contains(key)) {
+			throw new DuplicateKeyException("Key already exists in the dictionary");
+		}
+		keys.add(key);
+		values.add(value);
+	}
+	
+	public void remove(K key) {
+		int index = keys.indexOf(key);
+		keys.remove(index);
+		values.remove(index);
+	}
+	
+	public void update(K key, V value) {
+		values.set(keys.indexOf(key), value);
+	}
+	
+	public V lookup(K key) {
+		return values.get(keys.indexOf(key));
+	}
 }
